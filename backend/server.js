69aae41ee9c484090c2a875979b5ae429e31c554
Tamannatanwa <tamanna22@navgurukul.org>
2025-06-01 10:28:03 +0530

    error handling

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv")
 const chats = require("./data/data");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMidleware");
 const app = express()
 app.use(express.json())
 
@@ -16,7 +17,12 @@ app.get("/", (req, res) => {
     res.send("Hello Users!")
 })
 
+
+
+
 app.use("/api/user",userRoutes)
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
