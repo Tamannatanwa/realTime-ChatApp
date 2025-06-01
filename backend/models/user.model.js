fd542de28b823a142938991e662939bd9c909356
Tamannatanwa <tamanna22@navgurukul.org>
2025-06-01 10:20:31 +0530

    login funtionality

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema({
     name: {
@@ -7,7 +8,8 @@ const userSchema = mongoose.Schema({
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     password: {
         type: String,
@@ -15,12 +17,29 @@ const userSchema = mongoose.Schema({
     },
     pic: {
         type: String,
-        required: true,
         default: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"
     },
-},{
-    timestamp:true
-})
+}, {
+    timestamps: true
+});
 
-const User = mongoose.model("User",userSchema)
-module.exports = User
+
+// Password comparison method
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv")
 const chats = require("./data/data");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 const app = express()
+app.use(express.json())
 
 dotenv.config();
+
+connectDB()
 
 // app.use(express.json())
 
@@ -11,16 +16,7 @@ app.get("/", (req, res) => {
     res.send("Hello Users!")
 })
 
-app.get("/api/chat", (req, res) => {
-    res.send(chats)
-})
-
-app.get("/api/chat/:id", (req, res) => {
-    const singleChat = chats.find((c) =>
-        c._id === req.params.id)
-    res.send(singleChat)
-})
-
+app.use("/api/user",userRoutes)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
